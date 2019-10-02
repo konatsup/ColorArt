e214205d1c02631c2543abeab3bc63c47200eab8
konatsup <konatsukomatsu@gmail.com>
2019-10-02 17:50:48 +0900

    Change color by scroll position

--- sketch.js
+++ sketch.js
@@ -1,50 +1,78 @@
 let rectWidth;
 let bgm;
-const rootPath = 'http://localhost:8100/'
+let diameter = 0;
+const ROOT_PATH = "http://localhost:8100/";
+let currentPosition = 0;
+const MAX_HEIGHT = 10000;
+const MAX_COLOR_COUNT = 8;
+let currentColorNumber = 0;
+const BASE_SECTION_HEIGHT = MAX_HEIGHT / MAX_COLOR_COUNT;
 
 function preload() {
-  bgm = loadSound(rootPath + 'assets/sound/melt.mp3');
+  bgm = loadSound(ROOT_PATH + "assets/sound/melt.mp3");
+  se1 = loadSound(ROOT_PATH + "assets/sound/water.mp3");
 }
 
-
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(displayWidth, displayHeight - 100);
   noStroke();
-  background(230);
   rectWidth = width / 4;
-
 }
 
 function draw() {
-  let keyIndex = -1;
-  if (key >= 'a' && key <= 'z') {
-    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+  colorMode(HSB);
+  print(currentColorNumber * (360 / MAX_COLOR_COUNT));
+  fill(currentColorNumber * (360 / MAX_COLOR_COUNT), 70, 100);
+  rect(0, 0, displayWidth, displayHeight);
+  fill(0, 0, 90);
+  rect(0, 0, displayWidth * (currentPosition / MAX_HEIGHT), 20, 50);
+
+  if (mouseIsPressed) {
+    ripple();
+  }
+  // background(230);
+}
+
+function mouseWheel(event) {
+  currentPosition += event.delta;
+
+  if (currentPosition > MAX_HEIGHT) {
+    currentPosition = MAX_HEIGHT;
   }
 
+  if (currentPosition < 0) {
+    currentPosition = 0;
+  }
+  currentColorNumber = Math.floor(currentPosition / BASE_SECTION_HEIGHT);
+  print("currentPosition: " + currentPosition);
+  print("colorNum: " + currentColorNumber);
 
+}
 
-  if (keyIndex === -1) {
-    // If it's not a letter key, clear the screen
-    background(230);
-    randFill_r = Math.floor(Math.random() * 255 + 1);
-    randFill_g = Math.floor(Math.random() * 255 + 1);
-    randFill_b = Math.floor(Math.random() * 255 + 1);
-    fill(randFill_r, randFill_g, randFill_b);
-  } else {
-    // randFill_r = Math.floor(Math.random() * 255 + 1);
-    // randFill_g = Math.floor(Math.random() * 255 + 1);
-    // randFill_b = Math.floor(Math.random() * 255 + 1);
-    // fill(randFill_r, randFill_g, randFill_b);
-    let x = map(keyIndex, 0, 25, 0, width - rectWidth);
-    rect(x, 0, rectWidth, height);
+function keyTyped() {
+  switch (key) {
+    case "a":
+      se1.play();
+      break;
   }
 }
 
 function mousePressed() {
   if (bgm.isPlaying()) {
     bgm.stop();
-
   } else {
-    bgm.play();
+    // bgm.setVolume(0.2);
+    // bgm.play();
   }
+}
+
+let inc = 10;
+
+function ripple() {
+  diameter += inc;
+
+  // noFill();
+  // strokeWeight(5);
+  // stroke(255, 204, 0);
+  // ellipse(mouseX, mouseY, diameter, diameter);
 }
