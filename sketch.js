a22a6e02c32a7ca3c218120f49a94569256a38b0
konatsup <konatsukomatsu@gmail.com>
2019-10-02 14:59:52 +0900

    Playing BGM

--- sketch.js
+++ sketch.js
@@ -1,12 +1,50 @@
+let rectWidth;
+let bgm;
+
+function preload() {
+  bgm = loadSound('http://localhost:8100/melt.mp3');
+}
+
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(displayWidth, displayHeight);
+  noStroke();
+  background(230);
+  rectWidth = width / 4;
+  bgm.play();
+
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  let keyIndex = -1;
+  if (key >= 'a' && key <= 'z') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+
+
+  if (keyIndex === -1) {
+    // If it's not a letter key, clear the screen
+    background(230);
+    randFill_r = Math.floor(Math.random() * 255 + 1);
+    randFill_g = Math.floor(Math.random() * 255 + 1);
+    randFill_b = Math.floor(Math.random() * 255 + 1);
+    fill(randFill_r, randFill_g, randFill_b);
+  } else {
+    // randFill_r = Math.floor(Math.random() * 255 + 1);
+    // randFill_g = Math.floor(Math.random() * 255 + 1);
+    // randFill_b = Math.floor(Math.random() * 255 + 1);
+    // fill(randFill_r, randFill_g, randFill_b);
+    let x = map(keyIndex, 0, 25, 0, width - rectWidth);
+    rect(x, 0, rectWidth, height);
+  }
+}
+
+function mousePressed() {
+  // if (bgm.isPlaying()) {
+  //   bgm.stop();
+
+  // } else {
+  //   bgm.play();
+  // }
 }
